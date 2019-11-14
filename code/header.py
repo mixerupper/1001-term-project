fa392df1418daf77fb6ad4b786bd1d8ce8c9d62f
Andrew Yeh <andrew.yeh.10@gmail.com>
2019-11-13 20:20:12 -0500

    Created header for programs and additional data import

---
+++ code/header.py
@@ -0,0 +1,17 @@
+data_root = ""
+
+def initialize_data_root(user):
+	global data_root
+
+	if user == "AY":
+		data_root = "C:/Users/Andrew/1001-term-project/data/"
+	if user == "AH":
+		data_root = "/Users/Alec/Documents/DSGA_1001_Homework/DSGA_1001_Final_Project/1001-term-project/data/"
+	if user == "LZ":
+		data_root = ""
+
+	return("Initialized data root")
+
+def world():
+	print("Hello world")
+	return()
