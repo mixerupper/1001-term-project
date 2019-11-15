f9f46eec33c18190ea15618242efac38f72b211b
Andrew Yeh <andrew.yeh.10@gmail.com>
2019-11-14 20:03:51 -0500

    Update cleaning code

--- code/header.py
+++ code/header.py
@@ -7,7 +7,7 @@ def initialize_data_root(user):
 		data_root = "C:/Users/Andrew/1001-term-project/data/"
 	if user == "AH":
 		data_root = "/Users/Alec/Documents/DSGA_1001_Homework/DSGA_1001_Final_Project/1001-term-project/data/"
-	if user == "LZ":
+	if user == "YZ":
 		data_root = ""
 
 	return("Initialized data root")
