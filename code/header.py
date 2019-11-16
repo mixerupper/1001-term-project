f4914fb2384830d1aaa864f1373674b35acff57d
Andrew Yeh <andrew.yeh.10@gmail.com>
2019-11-16 00:27:44 -0500

    Separate out cleaning phases of sec data. Start unsupervised clustering of tags

--- code/header.py
+++ code/header.py
@@ -1,16 +1,12 @@
-data_root = ""
-
 def initialize_data_root(user):
-	global data_root
-
 	if user == "AY":
-		data_root = "C:/Users/Andrew/1001-term-project/data/"
+		return("C:/Users/Andrew/1001-term-project/data/")
 	if user == "AH":
-		data_root = "/Users/Alec/Documents/DSGA_1001_Homework/DSGA_1001_Final_Project/1001-term-project/data/"
+		return("/Users/Alec/Documents/DSGA_1001_Homework/DSGA_1001_Final_Project/1001-term-project/data/")
 	if user == "YZ":
-		data_root = ""
-
-	return("Initialized data root")
+		return("")
+	else:
+		return("Manually modify the data_root variable")
 
 def world():
 	print("Hello world")
